b381c427db16d07ac55cd7acc59a2975aac62171
Daniel <daniel.presas@uoit.net>
2020-08-02 22:37:30 -0400

    Changed face detection to Haar Cascade (runs well in release)

--- Thermocheck/src/main.cpp
+++ Thermocheck/src/main.cpp
@@ -5,25 +5,32 @@
 
 #if EXAMPLE
 
-#include <Examples/3_ObjectDetection/3-3_AdaptiveThresholding.cpp>
+#include <Examples/4_FaceAndFeatureDetection/4-5_HaarCascadingFaceDetection.cpp>
 
 #else
 
 int main(int argc, char** argv) {
 
+#define HSV_SPLIT_DETECT 0
+#define HAAR_CASCADE 1
+
 	Logger::init();
 	using namespace cv;
 
 	auto cam = VideoCapture(1);
-
 	cv::namedWindow("Capture");
 
+	Timer timer("Face cascade real time");
+	
 	while(true) {
-		UMat frame, frameCopy;
+		UMat frame;
 		LOG_ASSERT(cam.read(frame), "Failed to read frame from capture device!");
 		cv::imshow("Capture", frame);
-		frameCopy = frame.clone();
+		
+		UMat frameCopy = frame.clone();
 
+#if HSV_SPLIT_DETECT
+		
 		UMat hsv; cv::cvtColor(frame, hsv, COLOR_BGR2HSV);
 		std::array<Mat, 3> hsvChannels; cv::split(hsv, hsvChannels);
 
@@ -38,10 +45,32 @@ int main(int argc, char** argv) {
 		
 		for(uint32_t i = 0; i < contours.size(); ++i) {
 			if(cv::contourArea(contours.at(i)) < 1000.0) continue;
-			cv::drawContours(frameCopy, contours, i, { (double)Random::UInt(0, 255), (double)Random::UInt(0, 255), (double)Random::UInt(0, 255) }, 2);
+			cv::drawContours(frameCopy, contours, i, { 255, 255, 255 }, 2);
 		}
 		cv::imshow("Contour Skin Detect", frameCopy);
 		
+#elif HAAR_CASCADE
+
+		UMat gray; cv::cvtColor(frame, gray, COLOR_BGR2GRAY);
+		auto faceCascade = CascadeClassifier("assets/haarcascade_frontalface_default.xml");
+		LOG_ASSERT(!faceCascade.empty(), "Failed to load cascade classifier!");
+
+		std::vector<Rect> faces;
+		{
+			timer.start();
+			faceCascade.detectMultiScale(gray, faces, 1.1, 5, 0, { 40, 40 });
+			timer.stop();
+		}
+		LOG_TRACE("Number of faces detected: {}", faces.size());
+
+		for(const auto& rect : faces) {
+			cv::rectangle(frameCopy, { rect.x, rect.y }, { rect.x + rect.width, rect.y + rect.height }, { 255 }, 4);
+		}
+
+		cv::imshow("Haar Cascade", frameCopy);
+		
+#endif
+
 		const auto ch = cv::waitKey(1);
 		if(ch == 27 || !static_cast<bool>(cv::getWindowProperty("Capture", WND_PROP_VISIBLE))) {
 			break;
